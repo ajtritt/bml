b3eb2d5c6301256b75eda1e3b4696234abc15900
ajtritt <ajtritt@lbl.gov>
2024-03-28 16:03:34 -0700

    update VAE code

--- src/bml/nn/models/vae.py
+++ src/bml/nn/models/vae.py
@@ -1,10 +1,12 @@
 import glob
+import math
 
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, random_split
 
 import lightning as L
+from lightning.pytorch.loggers import CSVLogger
 
 from .resnet import resnet10_encoder, resnet10_decoder
 from ..loader import InitFerroXDataset
@@ -52,7 +54,7 @@ class VAE(nn.Module):
         kl = kl.sum(-1)
         return kl
 
-    def forward(self, batch):
+    def forward(self, batch, annealer=None):
         #x, _ = batch
         x = batch
 
@@ -75,6 +77,9 @@ class VAE(nn.Module):
         # kl
         kl = self.kl_divergence(z, mu, std)
 
+        if annealer is not None:
+            kl = annealer(kl)
+
         # elbo
         elbo = (kl - recon_loss)
         elbo = elbo.mean()
@@ -82,10 +87,79 @@ class VAE(nn.Module):
         return elbo, kl.mean(), recon_loss.mean()
 
 
-class LightningModule(L.LightningModule):
+
+class Annealer:
+    """
+    This class is used to anneal the KL divergence loss over the course of training VAEs.
+    After each call, the step() function should be called to update the current epoch.
+    """
+
+    def __init__(self, total_steps, shape='cosine', baseline=0.0, cyclical=False, disable=False):
+        """
+        Parameters:
+            total_steps (int): Number of epochs to reach full KL divergence weight.
+            shape (str): Shape of the annealing function. Can be 'linear', 'cosine', or 'logistic'.
+            baseline (float): Starting value for the annealing function [0-1]. Default is 0.0.
+            cyclical (bool): Whether to repeat the annealing cycle after total_steps is reached.
+            disable (bool): If true, the __call__ method returns unchanged input (no annealing).
+        """
+        self.total_steps = total_steps
+        self.current_step = 0
+        self.cyclical = cyclical
+        self.shape = shape
+        self.baseline = baseline
+        if disable:
+            self.shape = 'none'
+            self.baseline = 0.0
+
+    def __call__(self, kld):
+        """
+        Args:
+            kld (torch.tensor): KL divergence loss
+        Returns:
+            out (torch.tensor): KL divergence loss multiplied by the slope of the annealing function.
+        """
+        out = kld * self.slope()
+        return out
+
+    def slope(self):
+        if self.shape == 'linear':
+            y = (self.current_step / self.total_steps)
+        elif self.shape == 'cosine':
+            y = (math.cos(math.pi * (self.current_step / self.total_steps - 1)) + 1) / 2
+        elif self.shape == 'logistic':
+            exponent = ((self.total_steps / 2) - self.current_step)
+            y = 1 / (1 + math.exp(exponent))
+        elif self.shape == 'none':
+            y = 1.0
+        else:
+            raise ValueError('Invalid shape for annealing function. Must be linear, cosine, or logistic.')
+        y = self.add_baseline(y)
+        return y
+
+    def step(self):
+        if self.current_step < self.total_steps:
+            self.current_step += 1
+        if self.cyclical and self.current_step >= self.total_steps:
+            self.current_step = 0
+        return
+
+    def add_baseline(self, y):
+        y_out = y * (1 - self.baseline) + self.baseline
+        return y_out
+
+    def cyclical_setter(self, value):
+        if value is not bool:
+            raise ValueError('Cyclical_setter method requires boolean argument (True/False)')
+        else:
+            self.cyclical = value
+        return
+
+
+class LitVAE(L.LightningModule):
     """Lightning Module aka Network model"""
 
-    def __init__(self, max_gate_shape, latent_dim=32):
+    def __init__(self, max_gate_shape, latent_dim=32, anneal_steps=20, cyclical_anneal=True):
         """
         Args:
             n_channels: The number of values in each FerroX grid. By default,
@@ -93,11 +167,12 @@ class LightningModule(L.LightningModule):
         """
         super().__init__()
         self.vae = VAE(max_gate_shape, latent_dim=latent_dim)
+        self.annealer = Annealer(total_steps=anneal_steps, cyclical=cyclical_anneal)
 
     def training_step(self, batch, batch_idx):
         # training_step defines the train loop.
         batch = batch[0]
-        elbo, kl, recon_loss = self.vae(batch)
+        elbo, kl, recon_loss = self.vae(batch, annealer=self.annealer)
         self.log('elbo', elbo)
         self.log('recon_loss', recon_loss)
         self.log('kl', kl)
@@ -120,7 +195,7 @@ class LightningModule(L.LightningModule):
 class LightningDataModule(L.LightningDataModule):
     """Lightning Data Module"""
 
-    def __init__(self, data_dir_glob, batch_size, max_gate_shape):
+    def __init__(self, data_dir_glob, max_gate_shape):
         """
         Args:
             data_dir_glob: The glob string for getting the FerroX run directories to
@@ -128,7 +203,6 @@ class LightningDataModule(L.LightningDataModule):
         """
         super().__init__()
         self.data_dir_glob = data_dir_glob
-        self.batch_size = batch_size
         self.max_gate_shape = max_gate_shape
 
     def prepare_data(self):
@@ -145,10 +219,18 @@ class LightningDataModule(L.LightningDataModule):
         )
 
     def train_dataloader(self):
-        return DataLoader(self.train, batch_size=self.batch_size)
+        return DataLoader(self.train,
+                          batch_size=32,
+                          shuffle=False,
+                          drop_last=False,
+                          num_workers=3)
 
     def val_dataloader(self):
-        return DataLoader(self.val)
+        return DataLoader(self.val,
+                          batch_size=32,
+                          shuffle=False,
+                          drop_last=False,
+                          num_workers=3)
 
     def test_dataloader(self):
         return DataLoader(self.test)
@@ -158,3 +240,48 @@ class LightningDataModule(L.LightningDataModule):
         # called on every process in DDP
         pass
 
+
+def main(argv=None):
+    import argparse
+    import glob
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_dir', help='the directory with training data. should have directorie starting with "it"')
+    parser.add_argument("experiment", type=str, help="the experiment name")
+    parser.add_argument("-o", "--outdir", type=str, help="the directory to save output to", default='.')
+    parser.add_argument('-C', '--checkpoint_dir', help='the directory to save checkpoints to', default=None)
+    #parser.add_argument('-s', '--seed', help='the seed to use for randomly splitting data', default=27, type=int)
+    parser.add_argument('-S', '--sanity', help='the directory to save checkpoints to',
+                        default=False, action='store_true')
+    parser.add_argument('-b', '--batch_size', type=int, help='you should not run this code if you need this explained')
+    parser.add_argument('-e', '--epochs', type=int, help='you should not run this code if you need this explained')
+    args = parser.parse_args()
+
+    if args.checkpoint_dir is None:
+        args.checkpoint_dir = args.data_dir
+
+
+    model = LitVAE((200, 200, 52))
+    data = LightningDataModule(f'{args.data_dir}/it*', )
+
+
+    # Init ModelCheckpoint callback, monitoring 'val_loss'
+    callbacks = [cb.ModelCheckpoint(monitor='elbo')]
+
+    targs = dict(
+            default_root_dir=args.checkpoint_dir,
+            enable_checkpointing=True,
+            callbacks=callbacks,
+            fast_dev_run=args.sanity,
+            accelerator="gpu" if torch.cuda.is_available() else "cpu",
+            devices=1,
+            logger=CSVLogger(args.outdir, name=args.experiment),
+            #callbacks=[EarlyStopping(monitor=model.val_metric, min_delta=0.001, patience=3, mode="min")])
+    )
+
+    # train with both splits
+    trainer = L.Trainer(**targs)
+    trainer.fit(model, data_module=data)
+
+if __name__ == '__main__':
+    main()
